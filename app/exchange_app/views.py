64342653d961e7f9f82cd62c6cd48b0061e4e3f2
Akiller <mendiaky@fel.cvut.cz>
2024-07-19 11:22:15 +0600

    Full working converter with API

--- app/exchange_app/views.py
+++ app/exchange_app/views.py
@@ -11,6 +11,21 @@ def exchange(request):
         context = {
             'currencies': currencies,
         }
+        return render(request, 'exchange_app/index.html', context)
+
+    if request.method == 'POST':
+        from_amount = float(request.POST.get('from_amount'))
+        from_curr = request.POST.get('from_curr')
+        to_curr = request.POST.get('to_curr')
+
+        converted = round((currencies[to_curr] / currencies[from_curr]) * from_amount,2)
+        context = {
+            'from_amount': from_amount,
+            'from_curr': from_curr,
+            'to_curr': to_curr,
+            'currencies': currencies,
+            'converted': converted,
+        }
+        return render(request, 'exchange_app/index.html', context)
 
 
-    return render(request, 'exchange_app/index.html',context)
