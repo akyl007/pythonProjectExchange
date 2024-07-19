3e0d229dea474a53a19ac076a570c1b23aa7b963
Akiller <mendiaky@fel.cvut.cz>
2024-07-19 10:56:28 +0600

    Adding currency API

--- app/exchange_app/urls.py
+++ app/exchange_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import exchange
+from . import views
 
 urlpatterns = [
-    path('', exchange, name='exchange'),
+    path('', views.exchange, name='exchange'),
 
 ]
--- app/exchange_app/views.py
+++ app/exchange_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def exchange(request):
-    name = 'mr. Anderson'
 
-    context = {
-        'name': name,
-    }
-    return render(request, 'exchange_app/index.html',context=context)
+    response = requests.get(url='https://v6.exchangerate-api.com/v6/93ac3c0d2fef5bb38d62ad7b/latest/USD').json()
+    currencies = response.get('conversion_rates')
+
+    if request.method == 'GET':
+        context = {
+            'currencies': currencies,
+        }
+
+
+    return render(request, 'exchange_app/index.html',context)
